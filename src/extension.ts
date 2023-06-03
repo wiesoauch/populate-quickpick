b0d259c65d47689795fcaf2e9581fd00ec88bf63
wiesoauch <juliamayer@me.com>
2023-06-03 15:04:19 +0200

    extension works with relative path from workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,29 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('populate-quickpick.quickPickFromList', async () => {
-        
-		// TODO
-		// get file from relative path of workspace
-		// use workspace variable and explanatory filename for txt
-		// https://code.visualstudio.com/docs/editor/variables-reference
-		const myFile = '/Users/juliamayer/orga/available_headers.txt';
 
-		const content = fs.readFileSync(myFile);
-		const myArray = content.toString().split("\n");
 
+        let myFile = "";
+        if (vscode.workspace.workspaceFolders !== undefined) {
+            let workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+            myFile = path.join(workspacePath, "scripts/temp/available_headers.txt");
+
+        } else {
+            vscode.window.showInformationMessage('No workspace is currently open');
+        }
+
+        vscode.window.showInformationMessage("QuickPick from: " + myFile);
+
+        const content = fs.readFileSync(myFile);
+        const myArray = content.toString().split("\n");
 
         const selection = await vscode.window.showQuickPick(myArray);
         if (selection) {
             // vscode.window.showInformationMessage(`You selected: ${selection}`);
-			return selection;
+            return selection;
         } else {
             vscode.window.showInformationMessage('No item selected');
         }
@@ -26,4 +32,4 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
